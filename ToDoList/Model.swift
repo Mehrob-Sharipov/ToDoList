0a26719d0389aaa622a90f8c854294e2597eb749
Mehrob <shmehrob16@gmail.com>
2022-07-29 05:07:53 +0500

    Initial Commit

---
+++ ToDoList/Model.swift
@@ -0,0 +1,52 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Sharipov Mehrob on 16.07.2022.
+//
+
+import Foundation
+
+var toDoItems: [[String:Any]] {
+    
+    set {
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String:Any]] {
+            return array
+        } else {
+            return []
+        }
+    }        
+}
+
+func addItem(nameItem: String, isComplited: Bool = false) {
+    toDoItems.append(["name":nameItem, "isComplited":isComplited])
+}
+
+func removeItem(at index: Int){
+    toDoItems.remove(at: index)
+}
+
+func moveItem (fromIndex: Int, toIndex: Int){
+    let from = toDoItems[fromIndex]
+    toDoItems.remove(at: fromIndex)
+    toDoItems.insert(from, at: toIndex)
+}
+
+
+
+
+
+
+
+
+func changeState(at item: Int) -> Bool{
+    toDoItems[item]["isComplited"] = !(toDoItems[item]["isComplited"] as! Bool)
+    return toDoItems[item]["isComplited"] as! Bool
+}
+
+
